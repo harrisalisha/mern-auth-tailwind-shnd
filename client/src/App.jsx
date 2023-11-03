7d7b1e81e4448853e4996e31e9615337807d0bf6
harrishalisha <noranalisha@gmail.com>
2023-11-03 12:06:17 +0000

    react-router-dom and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Route,Routes} from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-red-500'>Hello Vite</h1>
+      <BrowserRouter>
+         <Routes>
+             <Route index path='/' element={<Home />} />
+             <Route path='/signin' element={<Signin />} />
+             <Route path='/signup' element={<Signup />} />
+             <Route path='/about' element={<About />} />
+             <Route path='/profile' element={<Profile />} />
+         </Routes>
+      </BrowserRouter>
     </>
   )
 }
